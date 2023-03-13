12f8a79208c8040612bf8610ccae3e8ed6b23702
nabongbonge <jaeseugi.1927@gmail.com>
2023-03-14 00:57:32 +0900

    Redis를 이용한 랭킹보드
    Redis를 이용한 랭킹보드

--- src/main/java/com/redis/store/StoreController.java
+++ src/main/java/com/redis/store/StoreController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StoreController {
